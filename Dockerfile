dca8ddcc742656d518df67e0d120850d7a9d71d9
boshiro123 <b.shiroki@mail.ru>
2025-02-22 00:05:17 +0300

    Deploy settings

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:22
+FROM openjdk:23
 WORKDIR /app
 COPY target/support-0.0.1-SNAPSHOT.jar .
 CMD ["java", "-jar", "support-0.0.1-SNAPSHOT.jar"]
---
+++ scripts/local_run_start.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Создаем сеть, если она еще не существует
+docker network create support-network || true
+
+# Собираем проект
+echo "Сборка проекта Maven..."
+./mvnw clean package -DskipTests
+
+# Запускаем базу данных
+echo "Запуск PostgreSQL..."
+docker compose --env-file .env \
+  up -d support_db
+
+# Ждем готовности PostgreSQL
+./scripts/wait-for-it.sh localhost:${POSTGRESDB_LOCAL_PORT} --timeout=60 --strict -- echo "PostgreSQL готов"
+
+# Запускаем основное приложение
+echo "Запуск Spring Boot приложения..."
+docker compose --env-file .env \
+  up --build -d bot-service
+
+echo "Все сервисы успешно запущены!"
